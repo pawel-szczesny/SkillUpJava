263eec1ca5ae6bff1241b303d2ea187761b06032
pawel-szczesny <pawel.szczesny@capgemini.com>
2018-05-28 09:57:03 +0200

    clean_code - not yet

---
+++ src/clean_code/exercise1.java
@@ -0,0 +1,17 @@
+package clean_code;
+
+
+public class exercise1 {
+	public static void main(String[] args) {
+		exercise1 exOne = new exercise1();
+		
+		int Result = exOne.AddFourNumbers(1,2,3,4);
+		
+	}
+	
+	public int AddFourNumbers(int one, int two, int three, int four) {
+		return one + two + three + four;
+	}
+	
+	
+}
