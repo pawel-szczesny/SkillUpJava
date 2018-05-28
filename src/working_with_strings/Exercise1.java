2ba9d88d31cba9e8c6aa1ebb6865aed2c85e7c07
pawel-szczesny <pawel.szczesny@capgemini.com>
2018-05-28 09:36:43 +0200

    working with strings tasks

---
+++ src/working_with_strings/Exercise1.java
@@ -0,0 +1,7 @@
+package working_with_strings;
+
+
+public class Exercise1 {
+	public static void main(String[] args) {
+	}
+}
---
+++ src/working_with_strings/Exercise2.java
@@ -0,0 +1,5 @@
+package working_with_strings;
+
+public class Exercise2 {
+
+}
